1faa2518d1c365f511628f36c10a458ea751ab6a
Olumorothy <iseoluwa89@gmail.com>
2022-11-01 13:33:27 +0000

    returns a list of all articles,added react boostrap for styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import { UserContext } from "./context/UserContext";
+import SignIn from "./components/SignIn";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Articles from "./components/Articles";
 
 function App() {
-  return <div className="App"></div>;
+  const [user, setUser] = useState("Moroti");
+  return (
+    <BrowserRouter>
+      <UserContext.Provider value={{ user, setUser }}>
+        <div className="App">
+          {!user ? (
+            <SignIn />
+          ) : (
+            <>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/articles" element={<Articles />} />
+              </Routes>
+            </>
+          )}
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
